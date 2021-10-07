0ae3a929e879a6a88cfa2407881b8ca2e3f56e51
Mihir501 <90403253+Mihir501@users.noreply.github.com>
2021-10-07 17:17:45 +0530

    Create RecursiveArray.java

---
+++ array/RecursiveArray.java
@@ -0,0 +1,26 @@
+package sample;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Practical
+{
+    public static void main(String[] args)
+    {
+        LocalDateTime t1 = LocalDateTime.now();
+        System.out.println(t1);
+        System.out.println("Mihir501");
+        System.out.println("Practical\n");
+
+                Scanner input = new Scanner(System.in);
+                int product=1;
+                System.out.print("Enter five integers: ");
+                int[] list = new int[5];
+                for (int i = 0; i < list.length; i++)
+                {
+                    list[i] = input.nextInt();
+                    product *= list[i];
+                }
+                System.out.println("The Product of elements is " + product);
+            }
+        }
