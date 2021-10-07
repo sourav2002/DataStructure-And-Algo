bb7aab07c42355c62fe3a01c1b5557dbb3af1fcf
Sardor <54173813+Sardor-Karimboyev@users.noreply.github.com>
2021-10-07 16:33:46 +0500

    added CombSort

---
+++ SortingAndSearching/CombSort.java
@@ -0,0 +1,56 @@
+package SortingAndSearching;
+
+import java.util.Arrays;
+
+class CombSort
+{
+    int getNextGap(int gap)
+    {
+        gap = (gap * 10) / 13;
+        if (gap < 1)
+            return 1;
+        return gap;
+    }
+ 
+    void sort(int array[])
+    {
+        int n = array.length;
+ 
+        int gap = n;
+ 
+        boolean isSwapped = true;
+ 
+        while (gap != 1 || isSwapped == true)
+        {
+            gap = getNextGap(gap);
+ 
+            isSwapped = false;
+ 
+            for (int i=0; i<n-gap; i++)
+            {
+                if (array[i] > array[i+gap])
+                {
+                    int temp = array[i];
+                    array[i] = array[i+gap];
+                    array[i+gap] = temp;
+ 
+                    isSwapped = true;
+                }
+            }
+        }
+    }
+ 
+    public static void main(String args[])
+    {
+        CombSort ob = new CombSort();
+        int array[] = {7, 5, 2, 49, 4, -24, 31, -3, 0, 38};
+        
+        ob.sort(array);
+ 
+        System.out.println("sorted arrayay");
+
+        for (int i=0; i<array.length; ++i)
+            System.out.print(array[i] + " ");
+ 
+    }
+}
