c81bf29a0e07bbc91af3fcb64589885151a4346d
sourav2002 <sainisourav233@gmail.com>
2022-01-15 02:32:26 +0530

    complete hash function

--- hashing/HashTable.java
+++ hashing/HashTable.java
@@ -5,6 +5,10 @@ public class HashTable {
     private final int numOfBuckets;
     private int size;
 
+    public HashTable(){
+        numOfBuckets = 10;
+        buckets = new HashNode[10];
+    }
     public HashTable(int capacity){
         this.numOfBuckets = capacity;
         buckets = new HashNode[capacity];
@@ -36,7 +40,6 @@ public class HashTable {
     // 3 main methods
 
     public void put(Integer key, String value){
-
         if (key == null || value == null){
             throw new IllegalArgumentException("Key or value is null");
         }
@@ -57,11 +60,42 @@ public class HashTable {
     }
 
     public String get(Integer key){
+        if (key == null){
+            throw new IllegalArgumentException("Key is null");
+        }
+          int bucketIndex = getBucketIndex(key);
+          HashNode head = buckets[bucketIndex];
+          while (head != null){
+              if (head.key.equals(key)){
+                  return head.value;
+              }
+              head = head.next;
+          }
         return null;
     }
 
     public String remove(Integer key){
-        return null;
+        if (key == null){
+            throw new IllegalArgumentException("Key is null");
+        }
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        HashNode prev = null;
+        while (head != null){
+            if (head.key.equals(key)){
+                break; // because we only wants to get head and prev at their required position
+            }
+            prev = head;
+            head = head.next;
+        }
+        if (head == null) return null; // if head.key not found then head.next become null
+        size--;
+        if (prev == null){
+            buckets[bucketIndex] = head.next;
+        }else {
+            prev.next = head.next;
+        }
+        return head.value;
     }
 
     public static void main(String[] args) {
@@ -69,8 +103,16 @@ public class HashTable {
         table.put(105, "Tom");
         table.put(21, "Sana");
         table.put(21, "Mery");
+        table.put(101, "cat");
+        table.put(1, "sourav");
 
-        System.out.println(table.size());
+        System.out.println("size is = " + table.size());
+
+        System.out.println("get "+table.get(101));
+        System.out.println("get "+table.get(1));
+        System.out.println("removed "+table.remove(1));
+        System.out.println("get "+table.get(1));
+        System.out.println("get "+table.get(21));
 
     }
 
