815fd2458422309f23d8fac57902613e34251135
Anirudh <59328945+anirudh0806@users.noreply.github.com>
2022-10-04 11:52:44 +0530

    Update Trapping-RainWater.java

--- DynamicProgramming/Trapping-RainWater.java
+++ DynamicProgramming/Trapping-RainWater.java
@@ -1,44 +1,33 @@
-public int trap(int[] height) {
-       if(height == null || height.length == 1) {
-           return 0;
-       }
-       
-       int firstPillar = -1;
-       int trappedWater = 0;
-       
-       for(int i = 0; i < height.length; i++) {
-           if(firstPillar == -1) { //to find the first pillar
-               if(height[i] > 0) {
-                   firstPillar = i;                   
-               }
-               continue;
-           }
-           
-           if(height[i] >= height[firstPillar]) {              
-               for(int j = firstPillar + 1; j < i; j++) {                   
-                   trappedWater += height[firstPillar] - height[j];
-               }
-               firstPillar = i;
-           }           
-       }
-       
-       int lastUsedPillar = firstPillar;
-       firstPillar = -1;
-       for(int i = height.length - 1; i >= lastUsedPillar; i--) {
-           if(firstPillar == -1) { //to find the first pillar
-               if(height[i] > 0) {
-                   firstPillar = i;                   
-               }
-               continue;
-           }
-           
-           if(height[i] >= height[firstPillar]) {              
-               for(int j = firstPillar - 1; j > i; j--) {                   
-                   trappedWater += height[firstPillar] - height[j];
-               }
-               firstPillar = i;
-           }           
-       }
-       
-       return trappedWater;
-   }    
+
+
+class Test {
+	static int arr[]= new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+	static int findWater(int n)
+	{
+		int left[] = new int[n];
+
+		int right[] = new int[n];
+		int water = 0;
+
+		left[0] = arr[0];
+		for (int i = 1; i < n; i++)
+			left[i] = Math.max(left[i - 1], arr[i]);
+
+	
+		right[n - 1] = arr[n - 1];
+		for (int i = n - 2; i >= 0; i--)
+			right[i] = Math.max(right[i + 1], arr[i]);
+
+		
+		for (int i = 0; i < n; i++)
+			water += Math.min(left[i], right[i]) - arr[i];
+
+		return water;
+	}
+	public static void main(String[] args)
+	{
+
+		System.out.println(findWater(arr.length));
+	}
+}
