25c7c11b6f22b18a8ce038510577e39fdf2a00a2
Gurpreet-Saini <sainigp20@gmail.com>
2022-10-05 21:47:16 +0530

    updated

--- array/basic/ArrayListImplement.java
+++ array/basic/ArrayListImplement.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ArrayListImplement {
     public static void main(String[] args) {
         ArrayList<String> fruits = new ArrayList<>();
+
         fruits.add("apple");
         fruits.add("mango");
         System.out.println(fruits);
