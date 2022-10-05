77e665ef37771e6051dad207b62cf0c2e00fcef2
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2022-10-05 09:31:41 +0530

    Added file for mergeSort function in java

---
+++ LinkedListPackage/MergeSort.java
@@ -0,0 +1,53 @@
+// Linked List class
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; this.next = null; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class MergeSort {
+  // Method to find the middle element.
+  public ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+  }
+ 
+  // Method to merge the two linked lists
+  public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+  }
+
+  // Method for sorting the linked list
+  public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left,right);
+  }
+}
