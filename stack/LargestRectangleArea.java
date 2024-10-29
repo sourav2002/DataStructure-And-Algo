e5916e5866131b45ae9c97433075b4aa31fe215e
Sourav Saini <132645581+sourav-pocketful@users.noreply.github.com>
2024-10-29 23:42:30 +0530

    Create LargestRectangleArea.java

---
+++ stack/LargestRectangleArea.java
@@ -0,0 +1,30 @@
+// Problem Statement:
+// Given an array of integers representing the heights of bars in a histogram, find the area of the largest rectangle in the histogram.
+
+import java.util.Stack;
+
+public class LargestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int i = 0;
+
+        while (i < n) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i++);
+        }
+
+        while (!stack.isEmpty()) {
+            int height = heights[stack.pop()];
+            int width = n - stack.peek() - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+}
